8e807d9bcee60cb4aec7666ed034a8c70d5a84b0
ashkulkarni <ashishkulkarni@hotmail.com>
2022-07-21 22:08:20 +0100

    Controller Test Added

---
+++ src/test/java/com/example/demo/controller/DemoControllerTests.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class DemoControllerTests {
+    @Autowired
+    private DemoController demoController;
+    @Test
+    void testGetHello() {
+       assertEquals("Hello World", demoController.getHello());
+    }
+}
